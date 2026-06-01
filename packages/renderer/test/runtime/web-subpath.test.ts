185e8191e00e05636aa660b40357618535233d27
Dik Rana <dik.rana@mechanicalrock.io>
2026-06-01 11:29:05 +0800

    feat(renderer): export browser-safe ./web subpath

---
+++ packages/renderer/test/runtime/web-subpath.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "bun:test"
+import { renderPortableText } from "@not-a-cms/renderer/web"
+
+describe("@not-a-cms/renderer/web subpath", () => {
+  test("renderPortableText is importable from the web subpath", () => {
+    expect(renderPortableText([{ type: "paragraph", children: [{ type: "text", value: "x" }] }], "web")).toContain("<p>")
+  })
+})
